ba3fb00659fdaa6d89d1bc3c7cb9e43ee3d6ebd2
hr_kang <hyerim513@gmail.com>
2023-07-07 11:12:50 +0900

    [feature] read

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,24 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include "../DeviceDriver/DeviceDriver.cpp"
+#include "../DeviceDriver/FlashMemoryDevice.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_THAT(1, 1);
-  EXPECT_TRUE(true);
+using namespace testing;
+
+class MockDevice : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+TEST(TestCaseName, Read) {
+	MockDevice mock_device;
+	DeviceDriver driver{ &mock_device };
+
+	EXPECT_CALL(mock_device, read(100))
+		.Times(5)
+		.WillRepeatedly(Return(100));
+
+	std::cout << driver.read(100);
 }
